ca41801cd0887c82851370a627f5e0abd217ee1f
Mark R. Evans <mark@glfusion.org>
2018-07-29 10:44:46 -0500

    better permission checking

--- public_html/vote.php
+++ public_html/vote.php
@@ -16,19 +16,32 @@
 
 require_once '../lib-common.php';
 
-if (!COM_isAjax()) die('no no');
-
-if ( !isset($_CONF['rating_speedlimit']) ) {
-    $_CONF['rating_speedlimit'] = 15;
-}
+if (!COM_isAjax()) die();
 
 header("Cache-Control: no-cache");
 header("Pragma: nocache");
 
-if (!isset($_POST['id']) || (!isset($_POST['type']) )) die('invalid');
+if (!isset($_POST['id']) || (!isset($_POST['type']) )) die();
 
 $faq_id = COM_applyFilter($_POST['id'],true);
 $type   = COM_applyFilter($_POST['type']);
+
+$result = DB_query("SELECT * FROM {$_TABLES['faq_questions']} AS f LEFT JOIN {$_TABLES['faq_categories']} AS c ON f.cat_id=c.cat_id WHERE f.id = ".(int) $faq_id);
+if (DB_numRows($result) == 1) {
+    $faqRecord = DB_fetchArray($result);
+
+    $permission = COM_getEffectivePermission( $faqRecord['owner_id'],
+                                              $faqRecord['group_id'],
+                                              $faqRecord['perm_owner'],
+                                              $faqRecord['perm_group'],
+                                              $faqRecord['perm_members'],
+                                              $faqRecord['perm_anon']
+                                            );
+
+    if ($permission == 0) {
+        die();
+    }
+}
 
 switch ($type) {
     case 'yes' :
@@ -36,6 +49,9 @@ switch ($type) {
         break;
     case 'no' :
         DB_change($_TABLES['faq_questions'], 'helpful_no', 'helpful_no + 1', 'id', (int) $faq_id, '', true);
+        break;
+    default :
+        die();
         break;
 }
 
